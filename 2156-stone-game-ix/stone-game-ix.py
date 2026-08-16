07a9ec0383337a115e8a74fc3f0fe2ff764884d9
Konki Gaurav Sampath <gauravsampath77@gmail.com>
2026-08-16 16:50:36 +0530

    Time: 205 ms (7.14%) | Memory: 20.5 MB (0.00%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def stoneGameIX(self, stones):
+        count = collections.Counter(stone % 3 for stone in stones)
+        if count[0] % 2 == 0:
+            return min(count[1], count[2]) > 0
+        return abs(count[1] - count[2]) > 2
+            
+
+        
