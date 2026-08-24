3a350d6479839496f25e69f329918ab9b9c27eb5
Konki Gaurav Sampath <gauravsampath77@gmail.com>
2026-08-24 22:40:20 +0530

    Time: 83 ms (26.30%) | Memory: 13.2 MB (85.09%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findAnagrams(self, s, p):
+        cnt = Counter(p)
+        ans = []
+        l = 0
+        for r, c in enumerate(s):
+            cnt[c] -= 1
+            while cnt[c] < 0:  
+                cnt[s[l]] += 1 
+                l += 1
+            if r - l + 1 == len(p):  
+                ans.append(l)  
+        return ans
+        
