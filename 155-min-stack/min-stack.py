bc9bb0b61ab41541dfad7e96e791a30ab6a7f052
Konki Gaurav Sampath <gauravsampath77@gmail.com>
2026-08-31 20:29:11 +0530

    Time: 191 ms (28.37%) | Memory: 25.7 MB (9.94%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,22 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack= []
+
+    def push(self, val):
+       if not self.stack:
+            self.stack.append((val,val))
+       else:
+            curr_min = min(val,self.stack[-1][1])
+            self.stack.append((val,curr_min))
+        
+
+    def pop(self):
+        self.stack.pop()
+
+    def top(self):
+       return self.stack[-1][0]
+        
+
+    def getMin(self):
+        return self.stack[-1][1]        
