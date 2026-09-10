a8d124723bbdf431e919ed6b94b095eeedd6e89a
Konki Gaurav Sampath <gauravsampath77@gmail.com>
2026-09-10 20:37:27 +0530

    Time: 35 ms (72.60%) | Memory: 13.7 MB (30.04%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeKdigits(self, num, k):
+        st = list()
+        for n in num:
+            while st and k and st[-1] > n:
+                st.pop()
+                k -= 1
+            if st or n != '0':
+                st.append(n)
+        if k:
+			st = st[0:-k] 
+        return ''.join(st) or '0'
+        
