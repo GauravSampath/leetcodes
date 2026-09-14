9c8d60f30cbd6fb254ad104120dca09fc48c9316
Konki Gaurav Sampath <gauravsampath77@gmail.com>
2026-09-14 20:22:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (47.06%) - LeetSync

--- 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.py
+++ 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.py
@@ -1,3 +1,3 @@
 class Solution(object):
     def hasSpecialSubstring(self, s, k):
-        return any(len(list(g)) == k for _, g in groupby(s))
+        return any(sum(1 for _ in g) == k for _, g in groupby(s))
